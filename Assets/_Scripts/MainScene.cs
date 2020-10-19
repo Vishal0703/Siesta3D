6160c22d0f0cfbe8e71d7eac2ebe882d6a6d3519
Vishal Gupta <vishal7031311@gmail.com>
2020-10-19 20:42:40 +0530

    UI Buttons added -> back buttons and sound play buttons added, button functionalities added

---
+++ Assets/_Scripts/MainScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void loadVolley()
+    {
+        SceneManager.LoadScene("LevelVolley");
+    }
+
+    public void loadSeista()
+    {
+        SceneManager.LoadScene("LevelSeista");
+    }
+
+    public void goBack()
+    {
+        SceneManager.LoadScene("LevelBegin");
+    }
+    //public void loadCook()
+    //{
+    //    SceneManager.LoadScene("LevelCook");
+    //}
+}
