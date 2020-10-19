2ddefdbf7c3f7ae485d22f9fca1f0c71a037eea9
Vishal Gupta <vishal7031311@gmail.com>
2020-10-19 18:59:28 +0530

    Playable sound buttons added, button handler added

---
+++ Assets/_Scripts/ButtonHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlaySound()
+    {
+        AudioSource[] audios = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+        foreach (AudioSource aud in audios)
+            aud.Stop();
+
+        AudioSource _audio = GetComponent<AudioSource>();
+        _audio.Play();   
+    }
+}
