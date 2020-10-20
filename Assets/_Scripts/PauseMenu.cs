56e8ec6275bdfb93221610357cc05644729fdda9
Vishal Gupta <vishal7031311@gmail.com>
2020-10-20 21:11:31 +0530

    Pause Menu Added

---
+++ Assets/_Scripts/PauseMenu.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    private void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Canvas[] ui = FindObjectsOfType(typeof(Canvas)) as Canvas[];
+        foreach (Canvas cv in ui)
+            cv.enabled = true;
+
+        GetComponent<Canvas>().enabled = false;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    void Pause()
+    {
+       
+        Canvas[] ui = FindObjectsOfType(typeof(Canvas)) as Canvas[];
+        foreach (Canvas cv in ui)
+            cv.enabled = false;
+        
+        GetComponent<Canvas>().enabled = true;
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
