0e0b19570668c8754b8f282aa9b3a881ab0cf79e
Vishal Gupta <vishal7031311@gmail.com>
2020-10-20 16:23:19 +0530

    Crude AI agent added, player and agent restricted to their sides using layer collision matrix

--- Assets/_Scripts/Agent.cs
+++ Assets/_Scripts/Agent.cs
@@ -1,44 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; // include so we can load new scenes
-using UnityStandardAssets.CrossPlatformInput;
 
 public class Agent : MonoBehaviour
 {
-    // Start is called before the first frame update
-
-    [Range(0.0f, 10.0f)] // create a slider in the editor and set limits on moveSpeed
-    public float moveSpeed = 3f;
-
-    public float pushMag = 100f;
-
-    public float jumpForce = 600f;
+    public float speed, jumpForce;
+    public Transform ball, groundCheck;
+    public float ballOffset;
     public AudioClip jumpSFX;
-    public AudioClip hitSFX;
-    public Transform groundCheck;
+    
+
     public LayerMask whatIsGround;
 
-    Transform _transform;
-    Rigidbody _rigidbody;
-    Animator _animator;
-    AudioSource _audio;
-
+    bool isGrounded;
     float _vz, _vy;
-    bool isRunning = false;
-    bool isGrounded = false;
-    void Awake()
+    Rigidbody _rigidbody;
+    AudioSource _audio;
+    float _destinationPoint;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        _transform = GetComponent<Transform>();
-
-        _rigidbody = GetComponent<Rigidbody>();
-        if (_rigidbody == null) // if Rigidbody is missing
-            Debug.LogError("Rigidbody component missing from this gameobject");
-
-        _animator = GetComponent<Animator>();
-        if (_animator == null) // if Animator is missing
-            Debug.LogError("Animator component missing from this gameobject");
-
         _audio = GetComponent<AudioSource>();
         if (_audio == null)
         { // if AudioSource is missing
@@ -47,65 +29,36 @@ public class Agent : MonoBehaviour
             _audio = gameObject.AddComponent<AudioSource>();
         }
 
+        _rigidbody = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        _vy = _rigidbody.velocity.y;
 
-        //_vz = CrossPlatformInputManager.GetAxisRaw("Horizontal");
-
-        //// Determine if running based on the horizontal movement
-        //if (_vz != 0)
-        //{
-        //    isRunning = true;
-        //}
-        //else
-        //{
-        //    isRunning = false;
-        //}
-        //_vy = _rigidbody.velocity.y;
-
-        //// Check to see if character is grounded by raycasting from the middle of the player
-        //// down to the groundCheck position and see if collected with gameobjects on the
-        //// whatIsGround layer
-        //isGrounded = Physics.Raycast(_transform.position, groundCheck.position - _transform.position, (float)(1.0), whatIsGround);
-
-        //// Set the grounded animation states
-        ////_animator.SetBool("Grounded", isGrounded);
-
-        //if (isGrounded && CrossPlatformInputManager.GetButtonDown("Jump")) // If grounded AND jump button pressed, then allow the player to jump
-        //{
-        //    DoJump();
-        //    isGrounded = false;
-        //}
-
-        //if (CrossPlatformInputManager.GetButtonUp("Jump") && _vy > 0f)
-        //{
-        //    _vy = 0f;
-        //}
-
-        //// Change the actual velocity on the rigidbody
-        //_rigidbody.velocity = new Vector3(0, _vy, _vz * moveSpeed);
-    }
-
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Ball")
+        _destinationPoint = Mathf.Clamp(ball.position.z + ballOffset, 2.5f, 7f);
+        if (transform.position.z <= _destinationPoint + 0.1f && transform.position.z >= _destinationPoint - 0.1f && transform.position.y < ball.position.y)
         {
-            Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
-            Vector3 temp = collision.gameObject.GetComponent<Transform>().position - _transform.position;
-            temp = temp.normalized;
-            //rb.AddForce(new Vector3(0, temp.y*pushMag, temp.z*pushMag));
-            if (temp.z >= 0)
-                rb.AddForce(new Vector3(0, pushMag, pushMag));
-            else
-                rb.AddForce(new Vector3(0, pushMag, -pushMag));
-
-
+            isGrounded = Physics.Raycast(transform.position, groundCheck.position - transform.position, (float)(1.0), whatIsGround);
+            if (isGrounded)
+                DoJump();
+        }
+        else
+        {
+            var step = speed * Time.deltaTime;
+            if (_destinationPoint - transform.position.z < 0)
+            {
+                transform.Translate(0, 0, -step);
+            }
+            else if (_destinationPoint - transform.position.z > 0)
+            {
+                transform.Translate(0, 0, step);
+            }
         }
     }
+
 
     void DoJump()
     {
