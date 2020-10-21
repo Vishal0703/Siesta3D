48aa71bf85c7dfd324504ade310d1329faee3c48
Vishal Gupta <vishal7031311@gmail.com>
2020-10-21 20:31:33 +0530

    AI Agent and player controls changed, sounds included - added to pausemenu

--- Assets/_Scripts/AIAgent.cs
+++ Assets/_Scripts/AIAgent.cs
@@ -10,11 +10,15 @@ public class AIAgent : MonoBehaviour
 
     public float lookRadius = 1f;
     public Transform midobj;
+    public float jumpForce = 300f;
 
     Transform target;
     NavMeshAgent agent;
+    float _vy;
+    Rigidbody _rigidbody;
     void Start()
     {
+        _rigidbody = GetComponent<Rigidbody>();
         target = GameObject.FindGameObjectWithTag("Ball").transform;
         agent = GetComponent<NavMeshAgent>();
     }
@@ -24,10 +28,21 @@ public class AIAgent : MonoBehaviour
     {
         Vector3 midposition = midobj.position;
         float distance = Vector3.Distance(target.position, transform.position);
-        if (distance <= lookRadius && target.position.z > midposition.z + 0.5) 
+        if (distance <= lookRadius && target.position.z > midposition.z + 0.4) 
         {
             agent.SetDestination(target.position);
+            //DoJump();
         }
+    }
+
+    void DoJump()
+    {
+        // reset current vertical motion to 0 prior to jump
+        _vy = 0f;
+        // add a force in the up direction
+        _rigidbody.AddForce(new Vector3(0, jumpForce, 0));
+        // play the jump sound
+        //PlaySound(jumpSFX);
     }
 
     private void OnDrawGizmosSelected()
