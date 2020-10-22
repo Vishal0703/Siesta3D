2fe6e7ba8ec23253a745db555e1cabcc0221c662
Vishal Gupta <vishal7031311@gmail.com>
2020-10-22 16:06:45 +0530

    Sleeping animation added, character texture applied - design completed

---
+++ Assets/AnimationSetter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationSetter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animator _animator;
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        if (_animator == null) // if Animator is missing
+            Debug.LogError("Animator component missing from this gameobject");
+
+        _animator.SetBool("Sleep", true);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
