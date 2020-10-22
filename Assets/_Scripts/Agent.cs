4f643bb696e55bac5bbd2e3e9dba8de13e520bdb
Vishal Gupta <vishal7031311@gmail.com>
2020-10-22 17:09:00 +0530

    Player AI Improved, left and right invisible plane movement added - to prevent ball from being stuck

--- Assets/_Scripts/Agent.cs
+++ Assets/_Scripts/Agent.cs
@@ -8,13 +8,16 @@ public class Agent : MonoBehaviour
     public Transform ball, groundCheck;
     public float ballOffset;
     public AudioClip jumpSFX;
+    public float pushMag = 100f;
+    public Transform midobj;
     
 
     public LayerMask whatIsGround;
 
-    bool isGrounded;
+    bool isGrounded = true;
     float _vz, _vy;
     Rigidbody _rigidbody;
+    //AudioSource _audio;
     //AudioSource _audio;
     float _destinationPoint;
 
@@ -34,30 +37,80 @@ public class Agent : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        _vy = _rigidbody.velocity.y;
 
-        _destinationPoint = Mathf.Clamp(ball.position.z + ballOffset, 2.5f, 7f);
-        if (transform.position.z <= _destinationPoint + 0.1f && transform.position.z >= _destinationPoint - 0.1f && transform.position.y < ball.position.y)
-        {
-            isGrounded = Physics.Raycast(transform.position, groundCheck.position - transform.position, (float)(1.0), whatIsGround);
-            if (isGrounded)
-                DoJump();
-        }
-        else
-        {
-            var step = speed * Time.deltaTime;
-            if (_destinationPoint - transform.position.z < 0)
+    private void Update()
+    {
+        //ball.position;\
+        //if (ball.position.z > midobj.position.z)
+        //{
+            //isGrounded = Physics.Raycast(transform.position, groundCheck.position - transform.position, (float)(2.0), whatIsGround);
+            //if (ball.position.y >= transform.position.y && isGrounded && Vector3.Distance(ball.position, transform.position) <= ballOffset)
+            //    DoJump();
+            if ((Vector3.Distance(ball.position, transform.position) <= ballOffset) && (ball.position.z > midobj.position.z))
             {
-                transform.Translate(0, 0, -step);
+                Vector3 dir = (ball.position-transform.position).normalized;
+                transform.Translate(0, dir.y*speed*Time.deltaTime, dir.z*speed*Time.deltaTime);
             }
-            else if (_destinationPoint - transform.position.z > 0)
+            else if(((Vector3.Distance(ball.position, transform.position) > ballOffset) | (ball.position.z < midobj.position.z))&&(groundCheck.position.y>=0.1))
             {
-                transform.Translate(0, 0, step);
+                transform.Translate(0, -speed * Time.deltaTime, 0);
             }
+        //}
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if(other.gameObject.tag == "Ball" && other.gameObject.transform.position.z > midobj.position.z)
+    //    {
+    //        isGrounded = Physics.Raycast(transform.position, groundCheck.position - transform.position, (float)(2.0), whatIsGround);
+    //        if (ball.position.y <= (transform.position.y + ballOffset) && isGrounded)
+    //            DoJump();
+    //    }
+    //}
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 temp = collision.gameObject.GetComponent<Transform>().position - transform.position;
+            temp = temp.normalized;
+            //Debug.Log($"Direction of force {temp.y} , {temp.z}");
+            rb.AddForce(new Vector3(0, temp.y * pushMag, temp.z * pushMag));
+            //if(temp.z >= 0)
+            //    rb.AddForce(new Vector3(0, pushMag, pushMag));
+            //else
+            //    rb.AddForce(new Vector3(0, pushMag, -pushMag));
+
+
         }
     }
+
+    //void Update()
+    //{
+    //    _vy = _rigidbody.velocity.y;
+
+    //    _destinationPoint = Mathf.Clamp(ball.position.z + ballOffset, 2.5f, 7f);
+    //    if (transform.position.z <= _destinationPoint + 0.1f && transform.position.z >= _destinationPoint - 0.1f && transform.position.y < ball.position.y)
+    //    {
+    //        isGrounded = Physics.Raycast(transform.position, groundCheck.position - transform.position, (float)(2.0), whatIsGround);
+    //        if (isGrounded)
+    //            DoJump();
+    //    }
+    //    else
+    //    {
+    //        var step = speed * Time.deltaTime;
+    //        if (_destinationPoint - transform.position.z < 0)
+    //        {
+    //            transform.Translate(0, 0, -step);
+    //        }
+    //        else if (_destinationPoint - transform.position.z > 0)
+    //        {
+    //            transform.Translate(0, 0, step);
+    //        }
+    //    }
+    //}
 
 
     void DoJump()
@@ -66,12 +119,19 @@ public class Agent : MonoBehaviour
         _vy = 0f;
         // add a force in the up direction
         _rigidbody.AddForce(new Vector3(0, jumpForce, 0));
+        isGrounded = false;
         // play the jump sound
         //PlaySound(jumpSFX);
     }
 
-    void PlaySound(AudioClip clip)
+    //void PlaySound(AudioClip clip)
+    //{
+    //    _audio.PlayOneShot(clip);
+    //}
+
+    private void OnDrawGizmosSelected()
     {
-        //_audio.PlayOneShot(clip);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, ballOffset);
     }
 }
